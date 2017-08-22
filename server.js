188bcadadeb4d08664e2550ea71847664e6cb4a6
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-08-22 17:06:14 -0400

    Added page and person data

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
+// Vendor modules
 const express = require('express');
 const hbs     = require('hbs');
 
+// Project modules
+const data    = require('./server/models/data');
 
 const port = process.env.PORT || 3000;
 var app = express();
 
-//app.set('view engine', 'html');
-//app.engine('html', require('hbs')._express);
-
+// Allow use of html extension
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 
+// Static web files
 app.use(express.static(__dirname + '/views'));
 hbs.registerPartials(__dirname + '/views/partials');
 
+// Web requests
 app.get('/', (req, res) => {
   res.render('index.hbs', {
-    title: 'Module Essentials'
+    title: data.Page.title,
+    desc:  data.Page.description,
+    author: data.Person.name,
+    github: data.Person.github
   });
 });
 
---
+++ server/models/data.js
@@ -0,0 +1,15 @@
+var Person = {
+  name: 'Reni',
+  lastName: 'Salcedo',
+  github: 'renisalcedo'
+};
+
+var Page = {
+  title: 'Module Essentials',
+  description: 'Different module resources for building any Nodejs application. All different modules mentioned on the same page. Modules for building, testing and deploying.'
+};
+
+module.exports = {
+  Person,
+  Page
+};
